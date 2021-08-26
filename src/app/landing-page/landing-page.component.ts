6abb54877af241adb505014571e87fce7fb2ccf1
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-26 15:18:24 +0300

    updated the landing page component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { KenUser } from '../ken-gh-class/ken-user';
+import { KenGhService } from '../ken-gh-service/ken-gh.service';
+import { GhUserService } from '../github-search-service/gh-user.service';
+import { GhRepoSearchService } from '../github-search-service/gh-repo-search.service';
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  ken:KenUser | any;
 
-  constructor() { }
+
+  constructor(private user: KenGhService) { }
 
   ngOnInit(): void {
+    this.user.KenRequest();
+    this.ken = this.user.ken;
   }
 
 }
