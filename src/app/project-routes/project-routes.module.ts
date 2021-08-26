303f7840cf25db69a7974c7651b87eedb0b68a94
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-27 00:08:42 +0300

    updated the project routes

--- src/app/project-routes/project-routes.module.ts
+++ src/app/project-routes/project-routes.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { GhRepoDetailsComponent } from '../gh-repo-details/gh-repo-details.component';
+import { GhUserDetailsComponent } from '../gh-user-details/gh-user-details.component';
+import { KenProjectsComponent } from '../ken-projects/ken-projects.component';
 
 
 const routes: Routes = [
   {path: 'home', component: LandingPageComponent}, 
   {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'repositories', component:GhRepoDetailsComponent},
+  {path: 'users', component:GhUserDetailsComponent},
+  {path: 'projects', component:KenProjectsComponent},
+  
+
 ];
 
 @NgModule({
