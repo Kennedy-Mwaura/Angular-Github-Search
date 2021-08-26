09c18c8e46b38ba95d42e7d8b78ffc28836e33a1
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-27 00:03:10 +0300

    updated the highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  
+  textDeco(action:string){
+    this.el.nativeElement.style.backgroundColor = action;
+  }
+  @HostListener ('click') onClicks(){
+    this.textDeco("rgba(0, 0, 255,0.47)");
+  }
+    
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
 
 }
