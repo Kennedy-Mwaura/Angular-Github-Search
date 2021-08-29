88c9d5da456954b1c15d42a3804db332071c03e2
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-29 18:42:49 +0300

    fixed typos

--- src/app/project-routes/project-routes.module.ts
+++ src/app/project-routes/project-routes.module.ts
@@ -9,11 +9,11 @@ import { KenProjectsComponent } from '../ken-projects/ken-projects.component';
 
 const routes: Routes = [
   {path: 'home', component: LandingPageComponent}, 
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '**', redirectTo: '/home', pathMatch: 'full'},
   {path: 'repositories', component:GhRepoDetailsComponent},
   {path: 'users', component:GhUserDetailsComponent},
   {path: 'projects', component:KenProjectsComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'},
 
 
 ];
